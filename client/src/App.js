19bb5af857b704c9e4f66d1f5a98c6696343a4ba
Bartek K <kontener00@gmail.com>
2020-10-31 14:47:39 +0100

    add improvements

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import io from "socket.io-client";
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { taskName: "", tasks: [] };
+  }
+
+  componentDidMount() {
+    this.socket = io("http://localhost:8000");
+    this.socket.on('updateData', tasks => {
+      this.updateTasks(tasks);
+    });
+    this.socket.on('addTask', task => {
+      this.addTask(task);
+    });
+    this.socket.on('removeTask', id => {
+      this.removeTask(id);
+    });
+  }
+
+  updateTask(value) {
+    this.setState({ taskName: value });
+  };
+
+  addTask(newTask) {
+    this.setState({ tasks: [...this.state.tasks, newTask] });
+  };
+
+  removeTask(id, isLocal) {
+    this.setState({
+      tasks: this.state.tasks.filter(task =>
+        task.id !== id)
+    });
+    if (isLocal) {
+      this.socket.emit('removeTask', id);
+    }
+  }
+
+  updateTasks(taskList) {
+    this.setState({ tasks: taskList });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    const newTask = { name: this.state.taskName, id: uuidv4() };
+    this.addTask(newTask);
+    this.socket.emit('addTask', newTask);
+    this.setState({ taskName: '' });
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return (
+      <div className="App">
+        <header>
+          <h1>ToDoList.app</h1>
+        </header>
+
+        <section className="tasks-section" id="tasks-section">
+          <h2>Tasks</h2>
+
+          <ul className="tasks-section__list" id="tasks-list">
+            {tasks.map((task) => (
+              <li className="task" key={task.id}>
+                {task.name}
+                <button
+                  className="btn btn--red"
+                  onClick={() => this.removeTask(task.id, true)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <form id="add-task-form">
+            <input
+              className="text-input"
+              autoComplete="off"
+              type="text"
+              placeholder="Type your description"
+              id="task-name"
+              onChange={e => this.updateTask(e.target.value)}
+            />
+            <button
+              className="btn"
+              type="submit"
+              onClick={e => this.submitForm(e)}>
+              Add
+            </button>
+          </form>
+        </section>
+      </div>
+    );
+  }
 }
-
 export default App;
